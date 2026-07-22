5e661803311d00d6fe79003fd8f56953e6563b80
Reviewer <reviewer@test.local>
2026-07-22 21:39:36 +0200

    fix(magmuzzle): strip orphaned launcher magazines on factory-level weapon removal; RICH_GEAR leader AT-round compat guard

---
+++ Tools/LoadoutManager/Data/Ammunition/Implementations/AirToGroundWeapons/Rockets/GuidedRockets/EIGHTAT5BMP2ROUND.cs
@@ -0,0 +1,11 @@
+﻿public class EIGHTAT5BMP2ROUND : BaseAmmunition
+{
+    public EIGHTAT5BMP2ROUND()
+    {
+        AmmunitionTypes = new List<AmmunitionType>() { AmmunitionType.EIGHTAT5BMP2ROUND };
+        amountPerPylon = 1;
+        weaponDefinition = (InterfaceWeapon)EnumExtensions.GetInstance(WeaponType.KONKURSLAUNCHERSINGLE.ToString()) as BaseWeapon;
+        ammoDisplayName = "Konkurs";
+        costPerPylon = 9999999;
+    }
+}
